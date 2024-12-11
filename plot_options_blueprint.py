26d99eb0be5f2821378b1d4de9645cc2e6b5f9da
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-12-11 11:28:40 -0600

    create plot-options route is tested and works

--- plot_options_blueprint.py
+++ plot_options_blueprint.py
@@ -8,3 +8,23 @@ plot_options_blueprint = Blueprint('plot_options_blueprint', __name__)
 @plot_options_blueprint.route('/plot-options', methods=['GET'])
 def plot_options_index():
     return jsonify({"message": "plot options index lives here"})
+
+@plot_options_blueprint.route('/plot-options', methods=['POST'])
+def create_plot_option():
+    try:
+        new_plot_option = request.json
+        connection = get_db_connection()
+        cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cursor.execute("""
+                        INSERT INTO plot_options (name, description, season, price, store)
+                        VALUES (%s, %s, %s, %s, %s)
+                        RETURNING *
+                        """,
+                        (new_plot_option['name'], new_plot_option['description'], new_plot_option['season'], new_plot_option['price'], new_plot_option['store'])
+        )
+        created_plot_option = cursor.fetchone()
+        connection.commit()
+        connection.close()
+        return jsonify({"plot_option": created_plot_option}), 201
+    except Exception as error:
+        return jsonify({"error": str(error)}), 500
