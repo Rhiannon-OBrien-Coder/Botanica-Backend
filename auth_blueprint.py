0e1ede5e1c37cd447bd901f97ec924832c80ffdd
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-12-12 10:10:09 -0600

    all backend routes in place, moving to front end

--- auth_blueprint.py
+++ auth_blueprint.py
@@ -2,7 +2,7 @@ import os
 import jwt
 import bcrypt
 import psycopg2, psycopg2.extras
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, g
 from db_helpers import get_db_connection
 from auth_middleware import token_required
 
